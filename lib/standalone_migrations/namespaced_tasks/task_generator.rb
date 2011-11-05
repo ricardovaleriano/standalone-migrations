5af42255d98ff0a4729bd847a71cf6e25e0b9b79
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2011-11-05 20:50:48 -0200

    refactoring, enhancing tasks path name generation

--- lib/standalone_migrations/namespaced_tasks/task_generator.rb
+++ lib/standalone_migrations/namespaced_tasks/task_generator.rb
@@ -6,7 +6,7 @@ module StandaloneMigrations
 
       def generate_for_all_found_subprojects
         create_tasks_dir
-        generate_task_file_for_dirs(subprojects_dirs)
+        generate_task_files
       end
 
       private
@@ -19,18 +19,13 @@ module StandaloneMigrations
         FileUtils.mkdir_p tasks_dir_path unless File.directory? tasks_dir_path
       end
 
-      def subprojects_dirs
-        Discoverer.new.subdirs_config_file.map do |config_path|
-          File.expand_path("../", config_path)
-        end
-      end
-
-      def generate_task_file_for_dirs(dirs)
-        dirs.each do |dir|
+      def generate_task_files
+        Discoverer.new.subdirs_config_file.each do |config_path|
+          # File.expand_path(config_path)
           # saber o dir aqui é importante para colocar essa config
           # no arquivo da task mais prá frente...
           # (como saber de onde veio a config...)
-          file_name = Pathname.new(dir).basename.to_s + "_tasks.rb"
+          file_name = Pathname.new(config_path).dirname.to_s + "_tasks.rb"
           file_path = File.join(tasks_dir_path, file_name)
           FileUtils.touch(file_path) unless File.exists? file_path
         end
