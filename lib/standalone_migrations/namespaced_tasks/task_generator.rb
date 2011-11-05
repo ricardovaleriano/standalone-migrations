4c516f26a943a9a4aca9f389e8b25cc5d51d38d2
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2011-11-05 19:35:25 -0200

    refactoring task_generator

--- lib/standalone_migrations/namespaced_tasks/task_generator.rb
+++ lib/standalone_migrations/namespaced_tasks/task_generator.rb
@@ -5,6 +5,12 @@ module StandaloneMigrations
     class TaskGenerator
 
       def generate_for_all_found_subprojects
+        create_tasks_dir
+      end
+
+      private
+
+      def create_tasks_dir
         FileUtils.mkdir_p "tasks" unless File.directory? "tasks"
       end
 
