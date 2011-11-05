9beaceb25ac9aa52e7aae0b90a57f5b013935640
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2011-11-05 19:46:50 -0200

    skeleton impl for one task file for each subproject

--- lib/standalone_migrations/namespaced_tasks/task_generator.rb
+++ lib/standalone_migrations/namespaced_tasks/task_generator.rb
@@ -6,12 +6,21 @@ module StandaloneMigrations
 
       def generate_for_all_found_subprojects
         create_tasks_dir
+        configurators = subprojects_configurators
+        p configurators
       end
 
       private
 
       def create_tasks_dir
         FileUtils.mkdir_p "tasks" unless File.directory? "tasks"
+      end
+
+      def subprojects_configurators
+        subprojects_configuration = Discoverer.new.dirs_with_config_file
+        subprojects_configuration.map do |config_path|
+          "um configurator para cada arquivo de configuracao existente #{config_path}"
+        end
       end
 
     end
