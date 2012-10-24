926bea267742cd58459784a409f215548dd3971f
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2012-10-24 01:30:05 -0200

    oops, right place for env vars detection :bomb:

--- lib/standalone_migrations.rb
+++ lib/standalone_migrations.rb
@@ -11,9 +11,9 @@ require "standalone_migrations/generator"
 railtie_app_path = "#{lib_path}/standalone_migrations/minimal_railtie_config"
 APP_PATH = File.expand_path(railtie_app_path,  __FILE__)
 
-require "standalone_migrations/minimal_railtie_config"
-require "standalone_migrations/tasks"
-
 if !ENV["RAILS_ENV"]
   ENV["RAILS_ENV"] = ENV["DB"] || ENV["RACK_ENV"] || Rails.env || "development"
 end
+
+require "standalone_migrations/minimal_railtie_config"
+require "standalone_migrations/tasks"
