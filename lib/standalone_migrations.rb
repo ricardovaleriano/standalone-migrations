3b19ee0f10f847b1896c7c7db40a91d4e19e7033
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2012-10-25 18:51:53 -0200

    now you can set an alternative path via env var

--- lib/standalone_migrations.rb
+++ lib/standalone_migrations.rb
@@ -5,13 +5,27 @@ require "rubygems"
 require "rails"
 require "active_record"
 
+module StandaloneMigrations
+  # TODO: add a if !defined? mattr_accessor here
+  # and give an alternative implementation to this method
+  # since it's brought to us by active_support which
+  # is a dependency of active_record and not of
+  # standalone_migrations.
+  # Ricardo Valeriano
+  mattr_accessor :alternative_root_db_path
+  db_path = ENV["db_path"] || ENV["DB_PATH"]
+  StandaloneMigrations.alternative_root_db_path = db_path
+end
+
 require "standalone_migrations/configurator"
 require "standalone_migrations/generator"
 require "standalone_migrations/setup"
 
 setup = StandaloneMigrations::Setup.new
 setup.environment
-APP_PATH = setup.railtie_app_path
 
+APP_PATH = setup.railtie_app_path
 require "standalone_migrations/minimal_railtie_config"
+
+setup.paths
 require "standalone_migrations/tasks"
